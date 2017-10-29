b7cadee0942c4d4c4ca60633fad68e0f7f3e5bff
Sam Hartley <samhartley92@gmail.com>
2017-10-29 10:44:20 +0000

    Works with webpack pipeline

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+    entry: [
+        './src/styles/index.less',
+        './src/js/star_canvas.js'
+    ],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "less-loader"
+                }]
+            },
+            {
+                test: /\.(ttf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[ext]'
+                }
+            }
+        ]
+    },
+    watch: true
+};
