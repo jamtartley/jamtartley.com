0e2d1fede2bc9544267da995c3c8d837becaa4c6
Sam Hartley <samhartley92@gmail.com>
2017-10-29 12:38:37 +0000

    Can now create fractal tree

--- src/js/tree.js
+++ src/js/tree.js
@@ -1,7 +1,31 @@
+import Branch from "./branch";
+
 class Tree {
     constructor(pos) {
         this.branches = [];
-        branches.push(new Branch(pos, 100));
+        this.branches.push(new Branch(pos, 200, 0));
+    }
+
+    progress() {
+        for (let i = this.branches.length - 1; i >= 0; i--) {
+            let branch = this.branches[i];
+
+            if (branch.isFinished === true) {
+                continue;
+            }
+
+            let newBranches = branch.progress(2);
+
+            for (let newBranch of newBranches) {
+                this.branches.push(newBranch);
+            }
+        }
+    }
+
+    draw(context) {
+        for (let branch of this.branches) {
+            branch.draw(context);
+        }
     }
 }
 
--- src/js/tree_manager.js
+++ src/js/tree_manager.js
@@ -1,5 +1,26 @@
-class TreeManager {
+import Tree from "./tree";
+import Vector2D from "./vector_2d";
 
+class TreeManager {
+    constructor(treeCount) {
+        this.trees = [];
+
+        for (let i = 0; i < treeCount; i++) {
+            this.trees.push(new Tree(new Vector2D(window.innerWidth / 2, window.innerHeight)));
+        }
+    }
+
+    update() {
+        for (let tree of this.trees) {
+            tree.progress();
+        }
+    }
+
+    draw(context) {
+        for (let tree of this.trees) {
+            tree.draw(context);
+        }
+    }
 }
 
 export default TreeManager;
