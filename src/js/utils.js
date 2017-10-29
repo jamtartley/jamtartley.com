b757097f221b84b6fd9cf33cd252454d44e09f4d
Sam Hartley <samhartley92@gmail.com>
2017-10-29 14:20:40 +0000

    Tweak some values for aesthetics/performance

--- src/js/utils.js
+++ src/js/utils.js
@@ -32,6 +32,6 @@ export function getRandFloat(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-export function randBetween(min, max) {
+export function getRandInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
