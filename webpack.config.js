27cbebdbb37bd6955074dfc98f8384fbaec72b38
Sam Hartley <samhartley92@gmail.com>
2017-10-29 17:20:26 +0000

    Add styling files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: [
         './src/styles/index.less',
         './src/js/index.js'
+    ],
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: "Production"
+        })
     ],
     output: {
         filename: "bundle.js",
