a46a59fa23e568c5fa8459551d40f5cf664f7217
Sam Hartley <samhartley92@gmail.com>
2017-10-29 11:35:47 +0000

    Start work on fractal trees

---
+++ src/js/tree.js
@@ -0,0 +1,8 @@
+class Tree {
+    constructor(pos) {
+        this.branches = [];
+        branches.push(new Branch(pos, 100));
+    }
+}
+
+export default Tree;
---
+++ src/js/tree_manager.js
@@ -0,0 +1,5 @@
+class TreeManager {
+
+}
+
+export default TreeManager;
