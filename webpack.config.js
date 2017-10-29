1a5a646ca1e3c64200f604aa220db231644fcb2d
Sam Hartley <samhartley92@gmail.com>
2017-10-29 22:04:37 +0000

    Remove plugins from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,9 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: [
         './src/styles/index.less',
         './src/js/index.js'
-    ],
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: "Production"
-        })
     ],
     output: {
         filename: "bundle.js",
