50dea692071f442e6dd1f4c0d73454510ef600d2
Sam Hartley <samhartley92@gmail.com>
2020-12-27 19:11:38 +0000

    Add CV terminal item

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import colours, { accentColours } from "../../colours";
 
 import { getRandomElement } from "../../utils";
+import sizes from "../../sizes";
 import styled from "styled-components";
 
 interface IWrapperProps {
@@ -14,12 +15,16 @@ const Wrapper = styled.div<IWrapperProps>`
   background: ${colours.TERMINAL_BACKGROUND};
   color: ${colours.FOREGROUND};
   border: 4px solid ${props => props.borderColour};
-  font-size: 20px;
+  font-size: 25px;
   font-family: monospace;
   box-shadow: -4px 4px 1px 0 ${props => props.shadowColour};
   margin: 20px;
   padding: 10px;
   flex: 1;
+
+  @media (max-width: ${sizes.MOBILE}) {
+    font-size: 16px;
+  }
 `;
 
 export const Terminal: FC = ({ children }) => {
