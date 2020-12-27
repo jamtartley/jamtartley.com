0af550e36d6b8828f5c82edc6b4063d040fcc41f
Sam Hartley <samhartley92@gmail.com>
2020-12-27 13:56:42 +0000

    Refactor terminal output types to share styles

--- src/components/PersonalInfo/PersonalInfo.test.tsx
+++ src/components/PersonalInfo/PersonalInfo.test.tsx
@@ -3,7 +3,20 @@ import { render, screen } from "@testing-library/react";
 import { PersonalInfo } from "./PersonalInfo";
 import React from "react";
 
-test("Name is capitalised correctly", () => {
+beforeEach(() => {
   render(<PersonalInfo />);
+});
+
+test("Name is capitalised correctly", () => {
   expect(screen.getByText(/Sam Hartley/)).toBeInTheDocument();
+});
+
+test("City renders correctly", () => {
+  expect(screen.getByText(/Wimbledon/)).toBeInTheDocument();
+  expect(screen.getByText(/London/)).toBeInTheDocument();
+});
+
+test("Email set correctly", () => {
+  const email = "sam@jamtartley.com";
+  expect(screen.getByText(email).closest("a")).toHaveAttribute("href", `mailto:${email}`);
 });
