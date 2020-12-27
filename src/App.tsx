e8e1f177ee736c90a3a91488b5dec8f221ae379c
Sam Hartley <samhartley92@gmail.com>
2020-12-27 05:48:48 +0000

    Rebuild in React

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+import React, { FC } from "react";
+
+import { UnderConstruction } from "./pages/UnderConstruction";
+
+const App: FC = () => {
+  return <UnderConstruction />;
+};
+
+export default App;
