09953eecd51a3d1a24c9f08e6bc6ca51aa393676
Sam Hartley <sam@jamtartley.com>
2021-03-08 16:23:01 +0000

    Rewrite describe block for Bio tests

--- src/components/Bio/Bio.test.tsx
+++ src/components/Bio/Bio.test.tsx
@@ -4,20 +4,22 @@ import { Bio } from "./Bio";
 import { MemoryRouter } from "react-router-dom";
 import React from "react";
 
-beforeEach(() => {
-  render(<Bio />, { wrapper: MemoryRouter });
-});
+describe("[Bio]", () => {
+  beforeEach(() => {
+    render(<Bio />, { wrapper: MemoryRouter });
+  });
 
-test("Name is capitalised correctly", () => {
-  expect(screen.getByText(/Sam Hartley/)).toBeInTheDocument();
-});
+  it("capitalises the name correctly", () => {
+    expect(screen.getByText(/Sam Hartley/)).toBeInTheDocument();
+  });
 
-test("City renders correctly", () => {
-  expect(screen.getByText(/Wimbledon/)).toBeInTheDocument();
-  expect(screen.getByText(/London/)).toBeInTheDocument();
-});
+  it("renders the city correctly", () => {
+    expect(screen.getByText(/Wimbledon/)).toBeInTheDocument();
+    expect(screen.getByText(/London/)).toBeInTheDocument();
+  });
 
-test("Email link works correctly", () => {
-  const email = "sam@jamtartley.com";
-  expect(screen.getByText(email).closest("a")).toHaveAttribute("href", `mailto:${email}`);
+  it("correctly links to the email", () => {
+    const email = "sam@jamtartley.com";
+    expect(screen.getByText(email).closest("a")).toHaveAttribute("href", `mailto:${email}`);
+  });
 });
