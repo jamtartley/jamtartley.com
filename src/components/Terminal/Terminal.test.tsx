b14861807b3b79b78ebdaaa722c2ae800f1b1842
Sam Hartley <sam@jamtartley.com>
2021-03-08 16:26:43 +0000

    Add render tests for Terminal

---
+++ src/components/Terminal/Terminal.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+
+import React from "react";
+import { Terminal } from "./Terminal";
+
+describe("[Terminal]", () => {
+  const childText = "CHILD TEXT";
+  const header = "TERMINAL HEADER";
+
+  beforeEach(() => {
+    render(<Terminal header={header}>{childText}</Terminal>);
+  });
+
+  it("displays given header", () => {
+    screen.getByText(header);
+  });
+
+  it("renders children", () => {
+    screen.getByText(childText);
+  });
+});
