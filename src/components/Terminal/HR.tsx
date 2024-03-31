aea6d04362883baa82bf9e3a61b0278ca7b5f8bb
Sam Hartley <sam@jamtartley.com>
2024-03-31 11:58:13 +0100

    refactor: start rebuild with flam

--- src/components/Terminal/HR.tsx
+++
@@ -1,7 +0,0 @@
-import colours from "../../colours";
-import styled from "styled-components";
-
-export const HR = styled.hr`
-	border: 1px solid ${colours.FOREGROUND};
-	opacity: 0.1;
-`;
--- src/utils.ts
+++
@@ -1,3 +0,0 @@
-export const getRandomElement = <T>(arr: T[]): T => {
-	return arr[Math.floor(Math.random() * arr.length)];
-};
