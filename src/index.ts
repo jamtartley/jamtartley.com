43719e7c521290a1d2be147a9c9e95651a047b48
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2024-03-31 11:58:13 +0100

    refactor: start rebuild with flam

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { compile } from "@flam/compiler";
+import { writeFileSync } from "node:fs";
+
+const entryFile = "flam/index.flam";
+const output = compile(entryFile, {
+	bio: [
+		{ command: "whoami", output: "Sam Hartley" },
+		{ command: "find /earth -name $(whoami)", output: "/UK/London" },
+		{ command: "cat bio/about", output: "Senior software engineer @humaans.io" },
+		{ command: "cat bio/skills", output: "TS/JS | React (+Native) | REST/GraphQL | C# | *nix | AWS | Terraform" },
+		{
+			command: "history | grep cv",
+			output: "CV",
+			link: "https://cv-sam-hartley.s3.eu-west-2.amazonaws.com/cv.pdf",
+		},
+		{ command: "cat bio/gitlab", output: "Gitlab", link: "https://gitlab.com/jamtartley" },
+		{ command: "cat bio/linkedin", output: "Linkedin", link: "https://uk.linkedin.com/in/sam-hartley-bb39a823a" },
+		{ command: "cat bio/email", output: "sam@jamtartley.com", link: "mailto:sam@jamtartley.com" },
+	],
+});
+
+writeFileSync("public/index.html", output);
--- src/utils/testUtils.ts
+++
@@ -1,8 +0,0 @@
-import { screen } from "@testing-library/react";
-
-export const findInTextContent = (text: string) => {
-	// @ts-ignore
-	screen.getAllByText((_, node) => {
-		return node?.textContent?.includes(text);
-	});
-};
